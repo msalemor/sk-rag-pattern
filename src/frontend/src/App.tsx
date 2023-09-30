8d303acb52f6e79b10acae70474eaf6ad1bb9cdd
Alex Morales <alemor@microsoft.com>
2023-09-30 00:38:25 -0400

    Initial frontend

--- src/frontend/src/App.tsx
+++ src/frontend/src/App.tsx
@@ -1,58 +1,82 @@
-import { For, createSignal } from 'solid-js'
-import './App.css'
+//import './App.css'
 
-interface Message {
-  prompt: string
-  content: string
-  usage: any
-  deepContent: string
+import { createSignal } from "solid-js"
+import SolidMarkdown from "solid-markdown"
+
+interface ISettings {
+  max_tokens: string,
+  temperature: string,
+  limit: string,
+  minRelevance: string
 }
 
-const mockMessages = [
-  {
-    prompt: 'What is your name?',
-    content: 'My name is Solid',
-    usage: 'const [name, setName] = createSignal("Solid")',
-    deepContent: 'test'
-  }
-]
+const DefaultSettings: ISettings = {
+  max_tokens: "2000",
+  temperature: "0.3",
+  limit: "3",
+  minRelevance: "0.77"
+}
 
 function App() {
-  const [messages] = createSignal<Message[]>(mockMessages)
-
-  const updateMessage = (idx: number, text: any) => {
-    console.info(idx, text)
-    // const item = {
-    //   prompt: 'What is your name 2?',
-    //   content: text,
-    //   usage: 'const [name, setName] = createSignal("Solid")',
-    //   deepContent: 'Solid is a declarative JavaScript library for creating user interfaces.'
-    // }
-    // const json = JSON.stringify(messages())
-    // const newMessages = JSON.parse(json)
-    // newMessages[idx].deepContent = item
-    // setMessages(newMessages)
-  }
-
+  const [settings, setSettings] = createSignal(DefaultSettings)
+  const [query, setQuery] = createSignal("")
+  const [completion] = createSignal("# This is some markdown")
   return (
     <>
-      <h1>Messages</h1>
-      <For each={messages()}>
-        {(message) => (
-          <div class="message">
-            <div class="message__prompt">{message.prompt}</div>
-            <div class="message__content">{message.content}</div>
-            <div class="message__usage">{message.usage}</div>
-            <div class="message__deep-content">{message.deepContent}</div>
-            <button
-              onClick={() => updateMessage(0, 'New content')}
-            >Update</button>
+      <nav class='p-2 bg-blue-950 text-white font-semibold text-lg'>
+        Search your documents
+      </nav>
+      <nav class='p-2 bg-blue-900 text-white font-semibold text-lg flex flex-row space-x-2'>
+        <div class='space-x-2'>
+          <label>Max Tokens:</label>
+          <input type="text" class='px-1 w-16 text-black'
+            value={settings().max_tokens}
+            onInput={e => setSettings({ ...settings(), max_tokens: e.currentTarget.value })}
+          />
+        </div>
+        <div class='space-x-2'>
+          <label>Temperature:</label>
+          <input type="text" class='px-1 w-16 text-black'
+            value={settings().temperature}
+            onInput={e => setSettings({ ...settings(), temperature: e.currentTarget.value })}
+          />
+        </div>
+        <div class='space-x-2'>
+          <label>Limit:</label>
+          <input type="text" class='px-1 w-16 text-black'
+            value={settings().limit}
+            onInput={e => setSettings({ ...settings(), limit: e.currentTarget.value })}
+          />
+        </div>
+        <div class='space-x-2'>
+          <label>Relevance:</label>
+          <input type="text" class='px-1 w-16 text-black'
+            value={settings().minRelevance}
+            onInput={e => setSettings({ ...settings(), minRelevance: e.currentTarget.value })}
+          />
+        </div>
+      </nav>
+      <main class="container mx-auto">
+        <div class="p-3 flex flex-col w-full space-y-2">
+          <div class="flex flex-row">
+            <textarea class="p-2 border w-[calc(100%-70px)] mr-[5px]" rows={5}
+              placeholder="What is your question?"
+              value={query()}
+              onInput={e => setQuery(e.currentTarget.value)}
+            />
+            <button class="p-2 bg-blue-700 hover:bg-blue-600 text-white text-sm font-semibold w-[75px] ml-auto">Search</button>
           </div>
-        )}
-      </For>
-
+          <div class="p-2 bg-blue-200 w-[95%]">
+            Prompt
+          </div>
+          <div class="p-2 bg-blue-300 w-[95%] ml-auto">
+            <SolidMarkdown children={completion()} />
+          </div>
+        </div>
+      </main>
     </>
   )
 }
+
 
 export default App
